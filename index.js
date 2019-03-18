60116dedbe29b7f154dee33f2452c01b826b8996
Tami Churns <tchurns@paperlesspost.com>
2019-03-18 23:59:20 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
-// Write your solution here!
+const drivers = ["Milo","Otis","Garfield"]
+
+function destructivelyAppendDriver(name) {
+  drivers.push(name)
+}
+
+function destructivelyPrependDriver(name) {
+  drivers.unshift(name)
+}
+
+function destructivelyRemoveLastDriver(name) {
+  drivers.pop(name)
+}
+
+function destructivelyRemoveFirstDriver(name) {
+  drivers.shift(name)
+}
+
+function appendDriver(name) {
+  const driversCopy = drivers.slice() 
+  driversCopy.push(name)
+  return(driversCopy)
+}
+
+function prependDriver(name) {
+  const driversCopy2 = drivers.slice() 
+  driversCopy2.unshift(name)
+  return(driversCopy2)
+}
+
+function removeLastDriver(name) {
+  const driversCopy3 = drivers.slice() 
+  driversCopy3.pop(name)
+  return(driversCopy3)
+}
+
+function removeFirstDriver(name) {
+  const driversCopy4 = drivers.slice() 
+  driversCopy4.shift(name)
+  return(driversCopy4)
+}
+
+
+
+
